f248479e1354b45a9d7bc65597e4d9c550606c3e
Sofiane Paris <benzait.ingenieur@gmail.com>
2022-02-19 13:28:52 +0100

    first Inbox test done

--- boilerplate/compile.js
+++ boilerplate/compile.js
@@ -1,6 +1,6 @@
 const path = require('path') ;
 const fs = require('fs');
-const solc = require('solc')
+const solc = require('solc');
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 
--- boilerplate/test/Inbox.test.js
+++ boilerplate/test/Inbox.test.js
@@ -4,7 +4,7 @@ const option = {};
 const provider = ganache.provider(option);
 const Web3 = require('web3');
 const web3 = new Web3(provider);
-
+const {interface, bytecode} = require('../compile');
 
 
 
@@ -13,18 +13,31 @@ const web3 = new Web3(provider);
 //mocha will deploy a contract --> (a transaction will occur even in the test network)
 //so we need a account to do that, that why before each test we will select a account and then run the tests
 //iteratively
-beforeEach(() => {
-  console.log('OK');
+let accounts;
+let inbox;
+
+beforeEach(async () => {
   //getAccounts is a async function so we use then to fetch the acount we want
-  web3.eth.getAccounts().then(fetchedAccounts => {
-      console.log("fetchedAccounts");
-      console.log(fetchedAccounts);
-    });
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+                    .deploy({data: bytecode, arguments: ['Hi there']})
+                    .send({from: accounts[0], gas: '1000000'});
 });
 
 
 describe('Inbox Test',() => {
-  it('deploy a contract', () =>{});
+  it('deploy a contract test', () =>{
+    assert.ok(inbox.options.address);
+  });
+
+  it('constructor test', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there');
+  })
+
+  it('setMessgae function test', async () =>{
+    await inbox.methods.setMessage('bye').send({from: accounts[0]});
+  })
 });
 
 
